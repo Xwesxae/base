0196ac72da9ae4e7b938cd85d773ee35ee95dbb7
Xwesxae <239178571+Xwesxae@users.noreply.github.com>
2025-11-14 10:52:19 +0300

    Update 1. Создание БД и CRUD операции.py

--- 1. Создание БД и CRUD операции.py
+++ 1. Создание БД и CRUD операции.py
@@ -1,1 +1,36 @@
+import sqlite3
 
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+# Создание таблицы
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE
+)
+''')
+conn.commit()
+
+# CREATE
+cursor.execute("INSERT INTO users (name, email) VALUES (?, ?)", ("Ivan", "ivan@mail.ru"))
+cursor.execute("INSERT INTO users (name, email) VALUES (?, ?)", ("Maria", "maria@mail.ru"))
+conn.commit()
+
+# READ
+cursor.execute("SELECT * FROM users")
+print("Все пользователи:", cursor.fetchall())
+
+# UPDATE
+cursor.execute("UPDATE users SET email = ? WHERE name = ?", ("new@mail.ru", "Ivan"))
+conn.commit()
+
+# DELETE
+cursor.execute("DELETE FROM users WHERE name = ?", ("Maria",))
+conn.commit()
+
+cursor.execute("SELECT * FROM users")
+print("После удаления:", cursor.fetchall())
+
+conn.close()
